76e0e2b23be5b8ee04bb13614926ebcd86cd3edd
Willian Ferreira dos santos <50432534+willianferreirax@users.noreply.github.com>
2022-10-03 20:41:09 -0300

    Add files via upload

---
+++ my_store.dart
@@ -0,0 +1,21 @@
+import 'package:mobx/mobx.dart';
+
+part 'my_store.g.dart';
+
+class MyStore = _MyStore with _$MyStore;
+
+abstract class _MyStore with Store{
+
+  @observable
+  int numero = 0;
+
+  @action
+  void aumentarNumero(){
+    numero++;
+  }
+
+  @action
+  void diminuirNumero(){
+    numero--;
+  }
+}
